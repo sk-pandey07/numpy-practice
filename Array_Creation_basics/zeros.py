81b004ae91f789e48b15dd283acac9d42115aeac
shashikant kumar <sp8326441@gmail.com>
2026-02-13 17:36:19 +0530

    Added examples of NumPy zeros() for 1D and 2D arrays

---
+++ Array_Creation_basics/zeros.py
@@ -0,0 +1,9 @@
+# 03 - NumPy zeros() Function Example
+import numpy as np
+# Creating a 1D array of zeros (3 elements)
+arr = np.zeros(3)
+# Creating a 2D array of zeros (2 rows, 3 columns)
+arr2 = np.zeros((2, 3))
+
+print("1D Zero Array:", arr)
+print("2D Zero Array:\n", arr2)
