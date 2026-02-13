76bc15e371b34c60b1f389ab28bd8797b0c1e509
shashikant kumar <sp8326441@gmail.com>
2026-02-13 18:17:02 +0530

    Added examples of 1D, 2D, and 3D arrays with properties (shape, size, ndim)

---
+++ Array_Creation_basics/shape_size_ndim.py
@@ -0,0 +1,19 @@
+import numpy as np
+# create 1D array
+arr_1d = np.array([1,2,3,4])
+print(arr_1d)
+print("array shape:",arr_1d.shape)
+print("total element:",arr_1d.size)
+print("diamention of array:",arr_1d.ndim)
+# create 2D array
+arr_2d = np.array([[1,2],[3,4]])
+print(arr_2d)
+print("array shape:",arr_2d.shape)
+print("total element:",arr_2d.size)
+print("diamention of array:",arr_2d.ndim)
+# create 3D array
+arr_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(arr_3d)
+print("array shape:",arr_3d.shape)
+print("total element:",arr_3d.size)
+print("diamention of array:",arr_3d.ndim)
