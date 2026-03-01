7af834713bb6de762d781d8a20d50afb415935cb
shashikant kumar <sp8326441@gmail.com>
2026-03-01 18:34:40 +0530

    Create 01.py

---
+++ pracrice set/01.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.array([1,2,3,4])
+b = np.array([10,20,30,40])
+
+sum = a + b
+print(sum)
+
+sub = a - b
+print(sub)
+
+multi = a * b
+print(multi)
+
+div = a / b
+print(div)
