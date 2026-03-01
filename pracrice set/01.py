385be0a599ff78dbbf9a4a633306a08fb13bc8e6
shashikant kumar <sp8326441@gmail.com>
2026-03-01 19:01:32 +0530

    Add NumPy array transformation demo (flatten and transpose)

--- pracrice set/01.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-arr = np.arange(1,13).reshape(3,4)
-print(arr)
---
+++ pracrice set/numpy_array_transformations.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.arange(1,13).reshape(3,4)
+print(arr)
+
+new = arr.flatten()
+print(new)
+
+transpose = arr.T
+print(transpose)
