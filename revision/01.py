81e2fd2154dd447c384d0c91be62ccd0c9207a58
shashikant kumar <sp8326441@gmail.com>
2026-03-15 22:54:46 +0530

    Create 01.py

---
+++ revision/01.py
@@ -0,0 +1,10 @@
+import numpy as np
+matrix = np.array([[1,2,3],
+                   [4,5,6],
+                   [7,8,9]])
+print("before swaping")
+print(matrix)
+
+matrix[[0,2]] = matrix[[2,0]]
+print("after swaping")
+print(matrix)
